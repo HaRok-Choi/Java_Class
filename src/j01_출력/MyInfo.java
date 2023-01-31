40ae06dfdfea19921c978599655b7c6ff0d4ea77
harok <gkfhrwkd23@naver.com>
2023-01-31 17:50:01 +0900

    day0130

---
+++ src/j01_출력/MyInfo.java
@@ -0,0 +1,26 @@
+package j01_출력;
+
+public class MyInfo {
+	public static void main(String[] args) {
+		
+		char name1 = '최';
+		char name2 = '하';
+		char name3 = '록';
+		
+		int birth = 1002;
+		
+		String name4 = "최하록"; // String : 클래스 자료형
+		
+		System.out.print("이름 : ");
+		System.out.println(name1 + name2 + name3 + "");
+//		순서에따라 다름.
+		System.out.println(name4);
+		System.out.print("생일 : ");
+		System.out.println(birth);
+		
+		
+		
+		
+		
+	}
+}
