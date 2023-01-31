b499fe321fa922eb4f5396bb2a0f907a6baad431
harok <gkfhrwkd23@naver.com>
2023-01-31 17:50:38 +0900

    day0130

---
+++ src/j01_출력/PrintTest.java
@@ -0,0 +1,18 @@
+package j01_출력;
+
+public class PrintTest {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+		System.out.print("Hello, Java!");
+		System.out.print("최하록");
+		System.out.println("-----------");
+		
+		System.out.print("Hello, Java!\n");
+		System.out.print("최하록");
+		
+//		println은 줄바꿈 하라는 뜻.
+		/*
+		  장문주석
+		*/
+	}
+}
